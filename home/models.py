5c781845657e2d871536567e4e0117ab575f3fd5
Chronologius <145150703+Chronologius@users.noreply.github.com>
2026-05-03 12:10:41 +0800

    Initial commit

---
+++ home/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
+from wagtail.models import Page
+
+
+class HomePage(Page):
+    subtitle = models.CharField(max_length=250, blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+        FieldPanel("body"),
+    ]
