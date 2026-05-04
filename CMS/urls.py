7eeb2e922f93ed592823c60622df96e1d0a6942f
Eddy Minaj <idriyang.13@gmail.com>
2026-05-04 21:15:45 +0800

    Update urls.py

--- CMS/urls.py
+++ CMS/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -22,7 +23,10 @@ if settings.DEBUG:
     # Serve static files only in debug mode, but serve media in all environments.
     urlpatterns += staticfiles_urlpatterns()
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Always serve media files regardless of DEBUG setting
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
