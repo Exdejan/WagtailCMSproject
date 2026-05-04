367d240c5b730177056c48dddff60036b3a92e49
Eddy Minaj <idriyang.13@gmail.com>
2026-05-04 17:21:43 +0800

    lux9

--- CMS/settings/base.py
+++ CMS/settings/base.py
@@ -131,7 +131,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
