0bc2a0e899d39ec46ca756f24c58b5ade3c598f9
Eddy Minaj <idriyang.13@gmail.com>
2026-05-04 16:52:57 +0800

    lux3

--- CMS/settings/production.py
+++ CMS/settings/production.py
@@ -2,11 +2,7 @@ from .base import *
 
 DEBUG = False
 
-# ManifestStaticFilesStorage is recommended in production, to prevent
-# outdated JavaScript / CSS assets being served from cache
-# (e.g. after a Wagtail upgrade).
-# See https://docs.djangoproject.com/en/6.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STORAGES["staticfiles"]["BACKEND"] = "whitenoise.storage.CompressedStaticFilesStorage"
 
 try:
     from .local import *
